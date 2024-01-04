fd4350eb660bea09345d57f06f13beeea55f0062
opdo <52803560+netfledgling@users.noreply.github.com>
2024-01-03 20:54:03 -0800

    ch 1 exercises
    from how to think like a computer scientist - python

---
+++ books/tlacs/1.12.exercises.py
@@ -0,0 +1,23 @@
+# 1. hello dear mrs smith | The bird, blue: it's winter. 
+
+# 2.
+1 + 2
+1 * 2
+2 ** 2
+2 - 1
+2 / 1
+
+# 3.
+# 1 2 # results in error
+
+# 4. 
+print("hello")
+"hello" # to note differences in quote marks
+
+# 5.
+# cheese # results in error
+
+# 6.
+6 + 4 * 9 # ran in script i.e. this file - doesn't output - needs a print function
+6 + 4 * 9 # ran in command line (not here) - outputs 42
+print(6 + 4 * 9) # ran here, outputs 42
